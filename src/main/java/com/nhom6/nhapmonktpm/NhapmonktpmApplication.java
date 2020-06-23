1eeeef65a57dad733396f661dabf1a796ba26506
taitut <taibtph09320@fpt.edu.vn>
2020-06-23 10:30:44 +0700

    "Project nhap mon"

---
+++ src/main/java/com/nhom6/nhapmonktpm/NhapmonktpmApplication.java
@@ -0,0 +1,13 @@
+package com.nhom6.nhapmonktpm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhapmonktpmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NhapmonktpmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhom6/nhapmonktpm/NhapmonktpmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhom6.nhapmonktpm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NhapmonktpmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
